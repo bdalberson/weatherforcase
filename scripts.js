f02fa77a29abc8547bf2e9b46bbb4428a6a3afab
Brian <brian@computers-MacBook-Pro.local>
2023-04-07 18:03:48 -0700

    literally cant believe it but the search button works already

--- scripts.js
+++ scripts.js
@@ -1,18 +1,21 @@
-let queriedCity = "oakland"; // Example queried city name, replace this with your desired value
+
+
+let queriedCity = ""
 
 let queriedLat, queriedLon; // Declare variables to store latitude and longitude
 
 // First API call to get latitude and longitude for queried city
 
-
 const searchButton = document.getElementById("searchbutton"); 
-searchButton.addEventListener("click", function() {fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + queriedCity + ",ca,usa&limit=1&appid=947f373954b974834bc6986dec7c1dd0")
+searchButton.addEventListener("click", function(event) {event.preventDefault(),queriedCity = document.getElementById("searchterm").value , fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + queriedCity + ",ca,usa&limit=1&appid=947f373954b974834bc6986dec7c1dd0")
   .then(function (response) {
+    console.log(response.json)
     return response.json();
   })
   .then(function (response) {
     queriedLat = response[0].lat;
     queriedLon = response[0].lon;
+    
     // Second API call to get weather forecast using latitude and longitude
     return fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + queriedLat + "&lon=" + queriedLon + "&appid=947f373954b974834bc6986dec7c1dd0");
   })
@@ -26,4 +29,3 @@ searchButton.addEventListener("click", function() {fetch("http://api.openweather
 
 
 
-  
